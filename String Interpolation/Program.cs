b7744621766df66e7794739ebaae0fc63061f6f8
ThiyagarajanR <ThiyagarajanR@users.noreply.github.com>
2025-01-26 16:16:20 +0000

    String interpolation

---
+++ String Interpolation/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 100;
+float b = 250.0f;
+string c = "CSharp";
+
+Console.WriteLine("The value are {0}, {1} and {2}", a , b , c);
+
+Console.WriteLine($"The values are {a}, {b} and {c}");
+
+Console.WriteLine($"{c} is upper-case is {c.ToUpper()}");
+
+
