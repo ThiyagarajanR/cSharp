dbb49e266ac7126e8d50edb105dda47c629ffed5
ThiyagarajanR <ThiyagarajanR@users.noreply.github.com>
2025-01-26 20:36:59 +0000

    Initialize DateParsing project with sample date parsing functionality

---
+++ DateParsing/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] sampleDateTimes = {
+    "January 1, 2025 9:30 AM",
+    "1/1/2025",
+    "Jan 1, 2025, 7:30PM",
+    "Jan 1, 25",
+    "1/2025",
+    "1/1/ 7PM",
+    "Jan 1 '15",
+};
+
+
+foreach(string datestr in sampleDateTimes)
+{
+    DateTime result;
+    // TODO: Use the static class function TryParse to try parsing the dates
+    if(DateTime.TryParse(datestr, out result))
+    {
+        Console.WriteLine($"{datestr, - 25} get parsed as :{result}");
+    }
+    else
+    {
+        Console.WriteLine($"Could not parse {datestr}");
+    }
+}
