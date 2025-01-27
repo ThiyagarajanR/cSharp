3346d3bf0962d837063c5b43b6792fe552cf0762
ThiyagarajanR <ThiyagarajanR@users.noreply.github.com>
2025-01-27 13:03:16 +0000

    File Details

---
+++ CreatingFiles/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+const string filename = "TestFile.txt";
+
+// TODO: Create a new file - this will overwrite any existing file
+using(StreamWriter sw = File.CreateText(filename))
+{
+    sw.WriteLine("This is a text file.");
+}
+
+
+// TODO: Determine if a file exists
+
+Console.WriteLine(File.Exists(filename));
+
+if(File.Exists(filename))
+{
+    File.Delete(filename);
+}
+else{
+    using(StreamWriter sw = File.CreateText(filename))
+    {
+        sw.WriteLine("This is a text file.");
+    }
+}
+
+Console.WriteLine(File.Exists(filename));
+
+
